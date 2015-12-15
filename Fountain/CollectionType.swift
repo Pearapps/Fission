9606d1cfbf934e332aa55049f47854ff0781b246
Kenny <pearapps@gmail.com>
2015-12-15 02:05:00 -0500

    Adding Apply

--- Fountain/CollectionType.swift
+++ Fountain/CollectionType.swift
@@ -9,3 +9,13 @@
 public func <^> <T, U, C: CollectionType where C.Generator.Element == T>(@noescape transform: T throws -> U, collection: C) rethrows -> [U] {
     return try collection.map(transform)
 }
+
+extension CollectionType {
+    func apply<T>(transforms: [Self.Generator.Element -> T]) -> [T] {
+        return transforms.flatMap { return self.map($0) }
+    }
+}
+
+public func <*><T, U: CollectionType>(transforms: [U.Generator.Element -> T], collection: U) -> [T] {
+    return collection.apply <| transforms
+}
--- Fountain/Operators.swift
+++ Fountain/Operators.swift
@@ -11,6 +11,11 @@ infix operator <^> {
     precedence 130
 }
 
+infix operator <*> {
+    associativity left
+    precedence 130
+}
+
 infix operator >>- {
     associativity left
     precedence 110
