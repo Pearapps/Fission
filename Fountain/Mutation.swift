df07c8fdfb705b89301aaaeb53b11bdc1f4dc60a
Kenny <pearapps@gmail.com>
2015-12-15 20:48:27 -0500

    Adding mutability functions

---
+++ Fountain/Mutation.swift
@@ -0,0 +1,18 @@
+//
+//  Mutation.swift
+//  Fountain
+//
+//  Created by Kenneth Parker Ackerson on 12/15/15.
+//  Copyright © 2015 Kenneth Ackerson. All rights reserved.
+//
+
+/// Based on https://twitter.com/jckarter/status/675445367542968320
+func applied<T, U> (transform: inout T -> U -> ()) -> T -> U -> T {
+    return { a in
+        { b in
+            var mutable = a
+            transform(&mutable)(b)
+            return mutable
+        }
+    }
+}
