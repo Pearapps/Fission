474518661054930b8d9e3ae6784c94783e1a7e37
Kenny <pearapps@gmail.com>
2015-12-15 22:30:42 -0500

    reordering

--- Fountain/CollectionType.swift
+++ Fountain/CollectionType.swift
@@ -6,10 +6,6 @@
 //  Copyright © 2015 Kenneth Ackerson. All rights reserved.
 //
 
-public func <^> <T, U, C: CollectionType where C.Generator.Element == T>(@noescape transform: T throws -> U, collection: C) rethrows -> [U] {
-    return try collection.map(transform)
-}
-
 extension CollectionType {
     func apply<T> (transforms: [Self.Generator.Element -> T]) -> [T] {
         return transforms.flatMap { return self.map($0) }
@@ -18,4 +14,8 @@ extension CollectionType {
 
 public func <*> <T, U: CollectionType> (transforms: [U.Generator.Element -> T], collection: U) -> [T] {
     return collection.apply <| transforms
+}
+
+public func <^> <T, U, C: CollectionType where C.Generator.Element == T>(@noescape transform: T throws -> U, collection: C) rethrows -> [U] {
+    return try collection.map(transform)
 }
