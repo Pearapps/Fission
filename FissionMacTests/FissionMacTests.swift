9d4ca4748dca79b0f0cdbe88789734a4cf523434
Kenny <pearapps@gmail.com>
2015-12-15 23:31:15 -0500

    tests

---
+++ FissionMacTests/FissionMacTests.swift
@@ -0,0 +1,76 @@
+//
+//  FissionMacTests.swift
+//  FissionMacTests
+//
+//  Created by Kenneth Parker Ackerson on 12/15/15.
+//  Copyright © 2015 Kenneth Ackerson. All rights reserved.
+//
+
+import XCTest
+import FissionMac
+
+final class FissionMacTests: XCTestCase {
+    
+    func testMap() {
+        let array = ["1", "2"]
+        
+        func function(string: String) -> Int? {
+            return Int(string)
+        }
+        
+        let mapped = array.map(function).flatMap { return $0 }
+        
+        let other = (function <^> array) |> compact
+        
+        XCTAssert(other == mapped)
+    }
+    
+    func testFlatMap() {
+        
+        let array = ["1", "2"]
+        
+        func function(string: String) -> [String] {
+            return [string]
+        }
+        
+        let flatMapped = array.flatMap(function)
+        
+        let other = array >>- function
+        
+        XCTAssert(other == flatMapped)
+    }
+    
+    func testFunctionApplication() {
+        func function(string: String) -> Int {
+            return 22
+        }
+        
+        let result = "hello" |> function
+        XCTAssert(result == 22)
+    }
+    
+    func testFunctionApplicationTwo() {
+        func function(string: String) -> Int {
+            return 22
+        }
+        
+        let result = function <| "hello"
+        XCTAssert(result == 22)
+    }
+    
+    func testFunctionComposition() {
+        func timesThree(amount: Int) -> Int {
+            return amount * 3
+        }
+        
+        func count(string: String) -> Int {
+            return string.characters.count
+        }
+        
+        let m = count >>> timesThree >>> { return [$0] } >>> { return $0.reduce(0, combine: +) }
+        
+        XCTAssert(m("hi") == 6)
+    }
+    
+}
+
