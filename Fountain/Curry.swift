63052951711c3483592c38ad2ad2633686b7eef9
Kenny <pearapps@gmail.com>
2015-12-15 21:47:40 -0500

    Adding docs

--- Fountain/Curry.swift
+++ Fountain/Curry.swift
@@ -6,6 +6,13 @@
 //  Copyright © 2015 Kenneth Ackerson. All rights reserved.
 //
 
+/**
+Transforms a function with two arguments into two functions with one argument each.
+
+- parameter f: The function to curry.
+
+- returns: A curried version of the provided function.
+*/
 public func curry<T, U, V>(f: (T, U) -> V) -> T -> U -> V {
     return { x in
         return {
@@ -14,6 +21,13 @@ public func curry<T, U, V>(f: (T, U) -> V) -> T -> U -> V {
     }
 }
 
+/**
+ Transforms a function that returns a function into a function that takes two arguments.
+ 
+ - parameter f: The function to uncurry.
+ 
+ - returns: An uncurried version of the provided function.
+ */
 public func uncurry<T, U, V>(f: T -> U -> V) -> (T, U) -> V {
     return {
         return f <| $0 <| $1
