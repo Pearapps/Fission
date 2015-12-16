9c279fcfc5bbc79a449821a429e9b6d4277612ba
Kenny <pearapps@gmail.com>
2015-12-15 20:33:56 -0500

    iOS

--- Fountain-Mac/Fountain.h
+++ Fountain-Mac/Fountain.h
@@ -1,19 +1,15 @@
 //
-//  Fountain.h
+//  Fountain-Mac.h
 //  Fountain
 //
 //  Created by Kenneth Parker Ackerson on 12/15/15.
 //  Copyright © 2015 Kenneth Ackerson. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Fountain.
 FOUNDATION_EXPORT double FountainVersionNumber;
 
 //! Project version string for Fountain.
 FOUNDATION_EXPORT const unsigned char FountainVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Fountain/PublicHeader.h>
-
-
